1bae563023fcf1f2386abb204e78451dc881d89f
shivamani <g.shivamani39@gmail.com>
2024-09-14 16:28:39 +0530

    frezing the buttons

--- vite-app/src/App.jsx
+++ vite-app/src/App.jsx
@@ -1,22 +1,50 @@
 import React from 'react'
 import Die from './Die'
+import {nanoid} from 'nanoid'
 
 export default function App() {
   const [dices,setdices]=React.useState(allNewDice())
+  const [Tenzies,settenzies]=React.useState(false)
+
+  React.useEffect(()=>{
+      console.log("dice state changed")
+  },[dices])
+
   function allNewDice() {
     const newDice = []
     for (let i = 0; i < 10; i++) {
-        newDice.push(Math.ceil(Math.random() * 6))
+        newDice.push(genaratedice())
     }
     return newDice
   }
-  function Roll(){
-    setdices(allNewDice())
+  function genaratedice(){
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid()
+    }
   }
-  const newElement=dices.map(dice=>{return <Die value={dice}/>})
+  // console.log(dices)
+  function Roll(){
+    setdices(dices=>dices.map(dice=>{
+        return(dice.isHeld)
+        ?dice
+        :genaratedice()
+    }))
+  }
+  function onHold(mid){
+      console.log(mid);
+
+      setdices(olddices=>olddices.map(dice=>{
+        return (dice.id==mid?{...dice,isHeld:!dice.isHeld}:dice)
+  }))
+  }
+  const newElement=dices.map(dice=>{return <Die key={dice.id} onHold={()=>{onHold(dice.id)}} isHeld={dice.isHeld} value={dice.value}/>})
   return (
     
     <div className='main'>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className='Dies'>  
           {newElement}
       </div>
--- vite-app/src/Die.jsx
+++ vite-app/src/Die.jsx
@@ -1,9 +1,13 @@
 import React from "react"
 
 export default function Die(props){
+    const styles={
+        backgroundColor: (props.isHeld)?"#59E391" : "white"
+    }
+    // const change=React.useEffect(()=>{return props.onHold},[props.onHold])
     return (
         <div>
-            <div className="die-num">{props.value}</div>
+            <div onClick={props.onHold} className="die-num" style={styles}>{props.value}</div>
         </div>
     )
 }
